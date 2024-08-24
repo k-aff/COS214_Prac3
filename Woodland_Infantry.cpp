d781f4ebd4ac2b79f60646b9c1f2d47e7ba0bf51
Driya Govender <u23535793@tuks.co.za>
2024-08-24 16:49:57 +0200

    cpp files completed for woodlands

---
+++ Woodland_Infantry.cpp
@@ -0,0 +1,14 @@
+#include "Woodland_Infantry.h"
+
+#include <iostream>
+using namespace std; 
+
+void Woodland_Infantry::move()
+{
+    cout << "move() called from Woodland_Infantry" << endl; 
+}
+
+void Woodland_Infantry::attack()
+{
+    cout << "attack() called from Woodland_Infantry" << endl; 
+}
